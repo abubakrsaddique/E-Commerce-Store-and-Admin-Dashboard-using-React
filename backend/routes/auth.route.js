81cdc803eb98a39f94f351f2781420336cd2f55f
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-11-19 21:45:29 +0500

    product page added

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -13,6 +13,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
-router.get("/profile", protectRoute, getProfile);
+// router.get("/profile", protectRoute, getProfile);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
-// import authRoutes from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -12,7 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json);
 
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
