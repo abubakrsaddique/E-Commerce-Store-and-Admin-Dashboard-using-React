b1718d68bef5ebddc025ce4967aad945349043ef
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-11-19 21:27:46 +0500

    Auth page added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.route.js";
+// import authRoutes from "./routes/auth.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes);
+app.use(express.json);
+
+// app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
