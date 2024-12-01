39d8c5ab62ba15b84de4d44df775792b7762e4f2
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-12-01 21:23:57 +0500

    authentication added

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
-// import { useUserStore } from "../stores/useUserStore";
+import { useUserStore } from "../stores/useUserStore";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -12,8 +12,7 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
-  const loading = false;
-  //   const { signup, loading } = useUserStore();
+  const { signup, loading } = useUserStore();
 
   const handleSubmit = (e) => {
     e.preventDefault();
